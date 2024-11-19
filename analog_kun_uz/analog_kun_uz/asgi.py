bb80e3f2af80fb46dd9838f57d2cd7cdf9794311
Шохзод Кудратов <kudratovsahzod2@gmail.com>
2024-11-19 19:26:10 +0300

    Initial commit

---
+++ analog_kun_uz/analog_kun_uz/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for analog_kun_uz project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'analog_kun_uz.settings')
+
+application = get_asgi_application()
